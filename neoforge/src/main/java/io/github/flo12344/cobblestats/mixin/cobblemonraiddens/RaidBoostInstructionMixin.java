8a2c1a168a008aa71d0c087e3425660cc7c0ff6a
Flouttre <florent.daerden38@gmail.com>
2026-02-01 20:59:26 +0100

    Rename + Added RaidDens ClearBoost sync

---
+++ neoforge/src/main/java/io/github/flo12344/cobblestats/mixin/cobblemonraiddens/RaidBoostInstructionMixin.java
@@ -0,0 +1,56 @@
+package io.github.flo12344.cobblestats.mixin.cobblemonraiddens;
+
+import com.cobblemon.mod.common.api.battles.interpreter.BattleMessage;
+import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
+import com.cobblemon.mod.common.api.pokemon.stats.Stats;
+import com.cobblemon.mod.common.battles.BattleRegistry;
+import com.cobblemon.mod.common.battles.interpreter.instructions.BoostInstruction;
+import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
+import com.necro.raid.dens.common.raids.RaidInstance;
+import com.necro.raid.dens.common.util.IRaidAccessor;
+import com.necro.raid.dens.common.util.IRaidBattle;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.cobblemon.mod.common.util.LocalizationUtilsKt.battleLang;
+
+
+@Mixin(BoostInstruction.class)
+public class RaidBoostInstructionMixin {
+    @Shadow(remap = false)
+    BattleMessage message;
+    @Shadow(remap = false)
+    BattlePokemon pokemon;
+
+    @Shadow
+    @Final
+    private boolean isBoost;
+
+    @Inject(method = "postActionEffect", at = @At("TAIL"), remap = false)
+    void RaidBoostInstructionMixin(PokemonBattle battle, CallbackInfo ci) {
+        if (!((IRaidBattle) battle).isRaidBattle()) return;
+        RaidInstance raid = ((IRaidBattle) battle).getRaidBattle();
+        BattlePokemon battlePokemon = this.pokemon;
+        if (battlePokemon == null || battlePokemon.getEntity() == null) return;
+        else if (!((IRaidAccessor) battlePokemon.getEntity()).isRaidBoss()) return;
+        if (battlePokemon.actor.getSide() == battle.getSide1()) return;
+        System.out.println(message.getRawMessage());
+
+        var severity = Stats.Companion.getSeverity(Integer.parseInt(message.argumentAt(2)));
+        var rootKey = (isBoost) ? "boost" : "unboost";
+
+        var stat = Stats.Companion.getStat(message.argumentAt(1)).getDisplayName();
+        var lang = message.hasOptionalArgument("zeffect") ? battleLang(rootKey + "." + severity + ".zeffect", pokemon.getName(), stat)
+                : battleLang(rootKey + "." + severity, pokemon.getName(), stat);
+        raid.getPlayers().forEach(serverPlayer -> {
+            var b = BattleRegistry.getBattleByParticipatingPlayer(serverPlayer);
+
+            if (b == battle) return;
+            b.broadcastChatMessage(lang);
+        });
+    }
+}
--- neoforge/src/main/java/io/github/flo12344/cobblestats/mixin/cobblemonraiddens/RaidClearBoostInstructionMixin.java
+++ neoforge/src/main/java/io/github/flo12344/cobblestats/mixin/cobblemonraiddens/RaidClearBoostInstructionMixin.java
@@ -2,14 +2,12 @@ package io.github.flo12344.cobblestats.mixin.cobblemonraiddens;
 
 import com.cobblemon.mod.common.api.battles.interpreter.BattleMessage;
 import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
-import com.cobblemon.mod.common.api.pokemon.stats.Stats;
 import com.cobblemon.mod.common.battles.BattleRegistry;
-import com.cobblemon.mod.common.battles.interpreter.instructions.BoostInstruction;
+import com.cobblemon.mod.common.battles.interpreter.instructions.ClearBoostInstruction;
 import com.cobblemon.mod.common.battles.pokemon.BattlePokemon;
 import com.necro.raid.dens.common.raids.RaidInstance;
 import com.necro.raid.dens.common.util.IRaidAccessor;
 import com.necro.raid.dens.common.util.IRaidBattle;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,36 +17,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static com.cobblemon.mod.common.util.LocalizationUtilsKt.battleLang;
 
 
-@Mixin(BoostInstruction.class)
-public class RaidBattleStartEventMixin {
+@Mixin(ClearBoostInstruction.class)
+public class RaidClearBoostInstructionMixin {
     @Shadow(remap = false)
     BattleMessage message;
-    @Shadow(remap = false)
-    BattlePokemon pokemon;
 
-    @Shadow
-    @Final
-    private boolean isBoost;
-
-    @Inject(method = "postActionEffect", at = @At("TAIL"), remap = false)
+    @Inject(method = "invoke", at = @At("TAIL"), remap = false)
     void RaidBoostInstructionMixin(PokemonBattle battle, CallbackInfo ci) {
         if (!((IRaidBattle) battle).isRaidBattle()) return;
         RaidInstance raid = ((IRaidBattle) battle).getRaidBattle();
-        BattlePokemon battlePokemon = this.pokemon;
+        BattlePokemon battlePokemon = message.battlePokemon(0, battle);
         if (battlePokemon == null || battlePokemon.getEntity() == null) return;
         else if (!((IRaidAccessor) battlePokemon.getEntity()).isRaidBoss()) return;
         if (battlePokemon.actor.getSide() == battle.getSide1()) return;
         System.out.println(message.getRawMessage());
 
-        var severity = Stats.Companion.getSeverity(Integer.parseInt(message.argumentAt(2)));
-        var rootKey = (isBoost) ? "boost" : "unboost";
-
-        var stat = Stats.Companion.getStat(message.argumentAt(1)).getDisplayName();
-        var lang = message.hasOptionalArgument("zeffect") ? battleLang(rootKey + "." + severity + ".zeffect", pokemon.getName(), stat)
-                : battleLang(rootKey + "." + severity, pokemon.getName(), stat);
+        var pokemonName = battlePokemon.getName();
+        var lang = battleLang("clearboost", pokemonName);
         raid.getPlayers().forEach(serverPlayer -> {
             var b = BattleRegistry.getBattleByParticipatingPlayer(serverPlayer);
-
             if (b == battle) return;
             b.broadcastChatMessage(lang);
         });
